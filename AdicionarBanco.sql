20efbf9190d0c0e6e369e36eb5d2f38dcf07e624
SalesIce1 <42521164+SalesIce1@users.noreply.github.com>
2020-03-29 18:03:34 -0300

    Atividade 3

---
+++ AdicionarBanco.sql
@@ -0,0 +1,5 @@
+USE Paises;
+INSERT INTO `Pais` (`id`,`nome`,`populacao`,`area`) VALUES (1,'Brasil',245.3,845641000);
+INSERT INTO `Pais` (`id`,`nome`,`populacao`,`area`) VALUES (2,'Estados Unidos',231.2,34044312);
+INSERT INTO `Pais` (`id`,`nome`,`populacao`,`area`) VALUES (3,'Lituania',10.29,32412);
+INSERT INTO `Pais` (`id`,`nome`,`populacao`,`area`) VALUES (4,'Estonia',41.64,514600);
